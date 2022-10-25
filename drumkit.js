ca85cff91b1f09371a3eff86a07cd5f47e335e03
Michal-Dudzik <dumichal20@wp.pl>
2022-10-26 00:22:34 +0200

    some parts are working

--- drumkit.js
+++ drumkit.js
@@ -1,13 +1,73 @@
-document.addEventListener('keypress', playsound);
+document.addEventListener('keypress', onKeyPress);
 
-function onKyePress(e) {
-	const key = e.key;
-	const sound = 'clap';
-	playsound(sound);
+const KeyToSound = {
+	a: 'hihat',
+	s: 'kick',
+	d: 'openhat',
+	f: 'boom',
+	g: 'ride',
+	h: 'snare',
+	j: 'tom',
+	k: 'tink',
+};
+
+const audio = new Audio('./sounds/hihat.wav');
+
+function onKeyPress(ev) {
+	// logika mapowania key -> sound
+	const sound = KeyToSound[ev.key];
+	switch (ev.key) {
+		case 'a':
+			sound = SOUND.clap;
+
+			// clap
+			break;
+		case 's':
+			sound = SOUND.hihat;
+			// hihat
+			break;
+		case 'd':
+			sound = SOUND.kick;
+			// kick
+			break;
+		case 'f':
+			sound = SOUND.openhat;
+			// openhat
+			break;
+		case 'g':
+			sound = SOUND.ride;
+			// ride
+			break;
+		case 'h':
+			sound = SOUND.snare;
+			// snare
+			break;
+		case 'j':
+			sound = SOUND.tink;
+			// tink
+			break;
+		case 'k':
+			sound = SOUND.tom;
+			// tom
+			break;
+	}
+	playSound(sound);
 }
 
-function playsound(e) {
-	const audiotag = document.querySelector('#' + sound);
-	audiotag.currentTime = 0;
-	audiotag.play();
+function playSound(sound) {
+	if (!sound) {
+		return;
+	}
+	const audioTag = document.querySelector(`#${clap}`);
+	audioTag.currentTime = 0;
+	audioTag.play();
 }
+// Date.now()
+
+document.getElementById('d1').addEventListener('mousedown', function () {
+	document.getElementById('d1').classList.add('keyPressed');
+	audio.play();
+});
+document.getElementById('d1').addEventListener('mouseup', function () {
+	document.getElementById('d1').classList.remove('keyPressed');
+});
